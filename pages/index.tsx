624eae57cf25b2eb9b03e0b93eee3f491675969b
Zypheraum <103285974+Zypheraum@users.noreply.github.com>
2023-06-09 04:02:51 +0530

    Updated Init

---
+++ pages/index.tsx
@@ -0,0 +1,128 @@
+import type { NextPage } from "next";
+import styles from "./index.module.css";
+const RaumBridgeMobile360x640: NextPage = () => {
+  return (
+    <div className={styles.raumBridgeMobile360x640}>
+      <img className={styles.whitelist11} alt="" src="/whitelist-1-1@2x.png" />
+      <div className={styles.groupParent}>
+        <div className={styles.frameParent}>
+          <div className={styles.byRaumNetworkAllRightsReParent}>
+            <div className={styles.byRaumNetworkContainer}>
+              <span className={styles.byRaumNetworkContainer1}>
+                <p className={styles.byRaumNetwork}>
+                  © 2023 by Raum Network. All rights reserved.
+                </p>
+              </span>
+            </div>
+            <img className={styles.frameChild} alt="" src="/line-2.svg" />
+          </div>
+          <div className={styles.frameWrapper}>
+            <div className={styles.frameGroup}>
+              <div className={styles.groupWrapper}>
+                <div className={styles.groupContainer}>
+                  <div className={styles.rectangleParent}>
+                    <input className={styles.groupChild} type="text" />
+                    <div className={styles.div}>1000</div>
+                  </div>
+                  <button className={styles.buttonprimarywithIcon}>
+                    <div className={styles.buttonNameParent}>
+                      <div className={styles.buttonName}>Connect</div>
+                      <div className={styles.icon}>
+                        <img
+                          className={styles.iconoutlinearrowRight}
+                          alt=""
+                          src="/iconoutlinearrowright.svg"
+                        />
+                      </div>
+                    </div>
+                  </button>
+                  <div className={styles.rnPolygon}>RN - Polygon</div>
+                  <img
+                    className={styles.raumLogo640640Round2}
+                    alt=""
+                    src="/raum-logo640-640-round-2@2x.png"
+                  />
+                </div>
+              </div>
+              <div className={styles.frame}>
+                <div className={styles.transactionFee5MaticNetParent}>
+                  <div className={styles.transactionFee5}>
+                    Transaction fee: 5 MATIC + Network Fees (Polygon)
+                  </div>
+                  <button className={styles.buttonprimarywithIcon1}>
+                    <div className={styles.buttonNameGroup}>
+                      <div className={styles.buttonName1}>Swap</div>
+                      <div className={styles.icon}>
+                        <img
+                          className={styles.iconoutlinearrowRight1}
+                          alt=""
+                          src="/iconoutlinearrowright1.svg"
+                        />
+                      </div>
+                    </div>
+                  </button>
+                </div>
+              </div>
+              <div className={styles.groupFrame}>
+                <form className={styles.rectangleGroup}>
+                  <input className={styles.groupItem} type="text" />
+                  <div className={styles.xasd}>0xasd..</div>
+                  <button className={styles.buttonprimarywithIcon2}>
+                    <div className={styles.buttonNameParent}>
+                      <div className={styles.buttonName}>Approve</div>
+                      <div className={styles.icon}>
+                        <img
+                          className={styles.iconoutlinearrowRight}
+                          alt=""
+                          src="/iconoutlinearrowright2.svg"
+                        />
+                      </div>
+                    </div>
+                  </button>
+                  <div className={styles.rnStarknet}>RN - StarkNet</div>
+                  <img
+                    className={styles.raumLogo640640Round21}
+                    alt=""
+                    src="/raum-logo640-640-round-2@2x.png"
+                  />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.socials}>
+          <a
+            className={styles.guildiconblack1}
+            href="https://guild.xyz/raum-network"
+            target="_blank"
+          />
+          <a
+            className={styles.icons8Telegram1501}
+            href="https://t.me/+zyKycRJuAhhjMDg1"
+            target="_blank"
+          />
+          <img
+            className={styles.icons8Discord25011}
+            alt=""
+            src="/icons8discord250-1-1@2x.png"
+          />
+          <a
+            className={styles.icons8Twitter4801}
+            href="https://twitter.com/RaumNetwork"
+            target="_blank"
+          />
+        </div>
+      </div>
+      <div className={styles.component1}>
+        <img
+          className={styles.raumLogo640640Round1}
+          alt=""
+          src="/raum-logo640-640-round-1@2x.png"
+        />
+        <div className={styles.bridge}>Bridge</div>
+      </div>
+    </div>
+  );
+};
+
+export default RaumBridgeMobile360x640;
